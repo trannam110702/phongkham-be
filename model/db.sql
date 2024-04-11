8a4583e867d745d04626c79a4f9a104cff804f1b
nam <tnam5353@gmail.com>
2024-04-11 17:49:22 +0700

    nam up

--- model/db.sql
+++ model/db.sql
@@ -1,5 +1,18 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+CREATE TABLE
+    "session" (
+        "sid" varchar NOT NULL COLLATE "default",
+        "sess" json NOT NULL,
+        "expire" timestamp(6) NOT NULL
+    )
+WITH
+    (OIDS = FALSE);
+
+ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+
+CREATE INDEX "IDX_session_expire" ON "session" ("expire");
+
 --PEOPLE--
 CREATE TABLE
     people (
